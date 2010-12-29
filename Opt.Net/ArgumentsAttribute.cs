8f9632a68e1c1e9161c9467b06c9970ac0bc0ca3
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2010-12-29 16:59:46 +0100

    Added ArgumentsAttribute with tests.

---
+++ Opt.Net/ArgumentsAttribute.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+
+namespace Opt
+{
+    /// <summary>
+    /// This attribute can be applied to a property of type <see cref="Collection{T}"/> where the <c>T</c>
+    /// is of type <see cref="string"/>. Any leftover arguments that cannot be explicitly assigned to its
+    /// own property will be added to the target property. Only one property in a container can have this
+    /// attribute.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+    public class ArgumentsAttribute : BasePropertyAttribute
+    {
+        /// <summary>
+        /// When implemented in a descendant class, will validate the usage of the attribute
+        /// on the container type.
+        /// </summary>
+        /// <param name="containerType">
+        /// The <see cref="Type"/> of the container.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        /// <para><paramref name="containerType"/> is <c>null</c>.</para>
+        /// </exception>
+        public override void ValidateUsage(Type containerType)
+        {
+            if (containerType == null)
+                throw new ArgumentNullException("containerType");
+
+            int propertiesWithThisAttribute = (from property in containerType.GetProperties()
+                                               where property.IsDefined(typeof (ArgumentsAttribute), true)
+                                               select property).Count();
+
+            if (propertiesWithThisAttribute == 0)
+                throw new InvalidOperationException(string.Format(CultureInfo.InvariantCulture,
+                    "The ArgumentsAttribute attribute is validated against a different container type than originally passed in; this is an internal error (aka bug), type validated against was {0}",
+                    containerType));
+
+            if (propertiesWithThisAttribute > 1)
+                throw new InvalidOperationException(string.Format(CultureInfo.InvariantCulture,
+                    "The ArgumentsAttribute attribute is applied to more than one property in the type {0}",
+                    containerType));
+        }
+
+        /// <summary>
+        /// When implemented in a descendant class, will validate the usage of the attribute
+        /// on the property in a container.
+        /// </summary>
+        /// <param name="property">
+        /// The <see cref="PropertyInfo"/> of the property to validate against.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        /// <para><paramref name="property"/> is <c>null</c>.</para>
+        /// </exception>
+        public override void ValidateUsage(PropertyInfo property)
+        {
+            if (property == null)
+                throw new ArgumentNullException("property");
+
+            if (!property.IsDefined(typeof (ArgumentsAttribute), true))
+                throw new InvalidOperationException(string.Format(CultureInfo.InvariantCulture,
+                    "The ArgumentsAttribute attribute is validated against a property that does not have the attribute, this is an internal error (aka bug), the property validated against was {0}",
+                    property));
+            if (property.PropertyType != typeof (Collection<string>))
+                throw new InvalidOperationException(string.Format(CultureInfo.InvariantCulture,
+                    "The ArgumentsAttribute attribute is applied to a property that is not of type Collection<string>, but of {0}",
+                    property.PropertyType));
+        }
+    }
+}
---
+++ Opt.Net/BasePropertyAttribute.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Reflection;
+
+namespace Opt
+{
+    /// <summary>
+    /// This class is the base class for all other attribute classes in Opt.Net
+    /// that is applied to properties, to control how command line arguments are
+    /// assigned to properties on the container object.
+    /// </summary>
+    public abstract class BasePropertyAttribute : Attribute
+    {
+        /// <summary>
+        /// When implemented in a descendant class, will validate the usage of the attribute
+        /// on the container type.
+        /// </summary>
+        /// <param name="containerType">
+        /// The <see cref="Type"/> of the container.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        /// <para><paramref name="containerType"/> is <c>null</c>.</para>
+        /// </exception>
+        public abstract void ValidateUsage(Type containerType);
+
+        /// <summary>
+        /// When implemented in a descendant class, will validate the usage of the attribute
+        /// on the property in a container.
+        /// </summary>
+        /// <param name="property">
+        /// The <see cref="PropertyInfo"/> of the property to validate against.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        /// <para><paramref name="property"/> is <c>null</c>.</para>
+        /// </exception>
+        public abstract void ValidateUsage(PropertyInfo property);
+    }
+}
