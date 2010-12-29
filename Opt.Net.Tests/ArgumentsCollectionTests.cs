d4fafe9f3dc6fcdcf2b538b7ae62486560b4c668
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2010-12-29 17:23:02 +0100

    Added initial implementation of arguments collection class, with tests.

---
+++ Opt.Net.Tests/ArgumentsCollectionTests.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Opt.Tests
+{
+    [TestFixture]
+    public class ArgumentsCollectionTests : TestBase
+    {
+        [Test]
+        public void Constructor_NullArguments_ThrowsArgumentNullException()
+        {
+            IEnumerable<string> arguments = null;
+
+            Assert.Throws<ArgumentNullException>(() => new ArgumentsCollection(arguments));
+        }
+
+        [Test]
+        public void Enumerate_CyclicResponseFiles_ThrowsInvalidOperationException()
+        {
+            string tempFileName1 = GetTempFileName();
+            string tempFileName2 = GetTempFileName();
+
+            File.WriteAllLines(tempFileName1, new[] { "2", "@" + tempFileName2, "3" });
+            File.WriteAllLines(tempFileName2, new[] { "2", "@" + tempFileName1, "3" });
+
+            IEnumerable<string> input = new[] { "1", "@" + tempFileName1, "4" };
+
+            Assert.Throws<InvalidOperationException>(() => new ArgumentsCollection(input).ToArray());
+        }
+
+        [Test]
+        public void Enumerate_ResponseFileListedTwice_ThrowsInvalidOperationException()
+        {
+            string tempFileName = GetTempFileName();
+
+            File.WriteAllLines(tempFileName, new[] { "2", "3" });
+
+            IEnumerable<string> input = new[] { "1", "@" + tempFileName, "@" + tempFileName, "4" };
+
+            Assert.Throws<InvalidOperationException>(() => new ArgumentsCollection(input).ToArray());
+        }
+
+        [Test]
+        public void Enumerate_WithEmptyResponseFile_ReturnsOriginalInputMinusResponseFileElement()
+        {
+            string tempFileName = GetTempFileName();
+            File.WriteAllText(tempFileName, String.Empty);
+
+            IEnumerable<string> input = new[] { "1", "@" + tempFileName, "2" };
+            IEnumerable<string> expected = new[] { "1", "2" };
+
+            string[] output = new ArgumentsCollection(input).ToArray();
+
+            CollectionAssert.AreEqual(expected, output);
+        }
+
+        [Test]
+        public void Enumerate_WithNestedResponseFiles_ReturnsOriginalInputWithContentsOfResponseFilesInline()
+        {
+            string tempFileName1 = GetTempFileName();
+            string tempFileName2 = GetTempFileName();
+
+            File.WriteAllLines(tempFileName1, new[] { "2", "@" + tempFileName2, "5" });
+            File.WriteAllLines(tempFileName2, new[] { "3", "4" });
+
+            IEnumerable<string> input = new[] { "1", "@" + tempFileName1, "6" };
+            IEnumerable<string> expected = new[] { "1", "2", "3", "4", "5", "6" };
+
+            string[] output = new ArgumentsCollection(input).ToArray();
+
+            CollectionAssert.AreEqual(expected, output);
+        }
+
+        [Test]
+        public void Enumerate_WithNullElements_ReturnsOriginalInputMinusNullElements()
+        {
+            IEnumerable<string> input = new[] { "1", "2", null, "3", "A", null, "B", "C", "", null };
+            IEnumerable<string> expected = new[] { "1", "2", "3", "A", "B", "C", "" };
+
+            string[] output = new ArgumentsCollection(input).ToArray();
+
+            CollectionAssert.AreEqual(expected, output);
+        }
+
+        [Test]
+        public void Enumerate_WithResponseFile_ReturnsOriginalInputWithContentsOfResponseFileInline()
+        {
+            string tempFileName = GetTempFileName();
+            File.WriteAllLines(tempFileName, new[] { "2", "3" });
+
+            IEnumerable<string> input = new[] { "1", "@" + tempFileName, "4" };
+            IEnumerable<string> expected = new[] { "1", "2", "3", "4" };
+
+            string[] output = new ArgumentsCollection(input).ToArray();
+
+            CollectionAssert.AreEqual(expected, output);
+        }
+
+        [Test]
+        public void Enumerate_WithSimpleArgumentsInput_ReturnsOriginalInput()
+        {
+            IEnumerable<string> arguments = new[] { "1", "2", "3", "A", "B", "C", "" };
+
+            string[] output = new ArgumentsCollection(arguments).ToArray();
+
+            CollectionAssert.AreEqual(arguments, output);
+        }
+
+        [Test]
+        public void NonGenericEnumerate_WithSimpleArgumentsInput_ReturnsOriginalInput()
+        {
+            IEnumerable<string> arguments = new[] { "1", "2", "3", "A", "B", "C", "" };
+
+            var collection = new ArgumentsCollection(arguments);
+            var enumerable = collection as IEnumerable;
+
+            var output = new List<string>();
+            foreach (string element in enumerable)
+                output.Add(element);
+
+            CollectionAssert.AreEqual(arguments, output);
+        }
+    }
+}
---
+++ Opt.Net/ArgumentsCollection.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+namespace Opt
+{
+    /// <summary>
+    /// This class will correctly recurse response files and produce all the arguments, in order. Any
+    /// <c>null</c>-elements will be ignored along the way.
+    /// </summary>
+    public class ArgumentsCollection : IEnumerable<string>
+    {
+        private readonly IEnumerable<string> _Arguments;
+        private readonly HashSet<string> _ProcessedResponseFiles = new HashSet<string>();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ArgumentsCollection"/> class.
+        /// </summary>
+        /// <param name="arguments">
+        /// The arguments to produce from.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        /// <para><paramref name="arguments"/> is <c>null</c>.</para>
+        /// </exception>
+        public ArgumentsCollection(IEnumerable<string> arguments)
+        {
+            if (arguments == null)
+                throw new ArgumentNullException("arguments");
+
+            _Arguments = arguments;
+        }
+
+        #region IEnumerable<string> Members
+
+        /// <summary>
+        /// Returns an enumerator that iterates through the collection.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="T:System.Collections.Generic.IEnumerator`1"/> that can be used to iterate through the collection.
+        /// </returns>
+        /// <filterpriority>1</filterpriority>
+        public IEnumerator<string> GetEnumerator()
+        {
+            foreach (string argument in _Arguments)
+            {
+                if (argument == null)
+                    continue;
+
+                if (argument.StartsWith("@"))
+                {
+                    foreach (string argumentFromResponseFile in ProcessResponseFile(argument.Substring(1)))
+                        yield return argumentFromResponseFile;
+                }
+                else
+                    yield return argument;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        #endregion
+
+        private IEnumerable<string> ProcessResponseFile(string fileName)
+        {
+            Debug.Assert(!StringEx.IsNullOrWhiteSpace(fileName));
+
+            fileName = Path.GetFullPath(fileName);
+            if (_ProcessedResponseFiles.Contains(fileName))
+                throw new InvalidOperationException(
+                    string.Format("Response file '{0}' has been processed more than once, this is not allowed", fileName));
+
+            _ProcessedResponseFiles.Add(fileName);
+            using (var reader = new StreamReader(fileName))
+            {
+                string argument;
+                while ((argument = reader.ReadLine()) != null)
+                {
+                    if (argument.StartsWith("@"))
+                    {
+                        foreach (string nestedResponseFileArgument in ProcessResponseFile(argument.Substring(1)))
+                            yield return nestedResponseFileArgument;
+                    }
+                    else
+                        yield return argument;
+                }
+            }
+        }
+    }
+}
