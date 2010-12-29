d5ac4c10ef8419194898cfcd6d183e70e6b51591
Lasse V. Karlsen <lasse@vkarlsen.no>
2010-12-29 15:10:11 +0100

    Initial commit.

---
+++ Opt.Net/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Opt.Net")]
+[assembly: AssemblyDescription("Command Line Parameter Parsing and Handling")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Opt.Net")]
+[assembly: AssemblyCopyright("Copyright (C) Lasse V. Karlsen 2010, All rights reserved")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("881bf89f-ebee-4670-88da-05e9b595d414")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: CLSCompliant(true)]
+#if USE_RELEASE_KEY
+[assembly:
+    InternalsVisibleTo(
+        "Opt.Net.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100b563320c3b6f283800943e1f6ff145efe4a7388595765b8e7a5382f8be763d133125018d416ea5e948fc7077c6486d22af28c3e1a7a8a3c0cbf5f8f10ab75d54990cd1bf156cb00c8c16274ff9f596c467f92212a5828ed97eeeb31b12c28f396cb8d1c178c7e10bf7ef015495c281a1c9110750a3cdebc34a0dc48eef6d25ab"
+        )]
+#else
+
+[assembly:
+    InternalsVisibleTo(
+        "Opt.Net.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100513ef4d842b98409e2d1f45338d19d2799a413953827e0f905bb11f860177b783654061534caa81bd45e24c2b22bdac3df30f93cfd9dafdf6b5e54c5793146554eca67a645e0a2e84229e222aa8324094642f76b309a85e5b42ac4cf6cf4a7ef24f9d037893fe7f9fdef26e5fd320464060dd9bd2b8b16836748383426027ff2"
+        )]
+#endif
+
+[assembly: NeutralResourcesLanguage("en")]
