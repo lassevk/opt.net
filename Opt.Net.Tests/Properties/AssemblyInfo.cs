c60b0fe88c34fb6d1076b3a34b0edd6d2931ffb8
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2010-12-29 15:10:11 +0100

    Initial commit.

---
+++ Opt.Net.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Opt.Net.Tests")]
+[assembly: AssemblyDescription("Tests for Opt.Net Class Library")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Opt.Net")]
+[assembly: AssemblyCopyright("Copyright (C) Lasse V. Karlsen 2010, All rights reserved")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("1e627b02-2fcc-4894-87cb-98230ef5d21a")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
---
+++ README.markdown
@@ -0,0 +1,14 @@
+This project contains code that will process command line arguments,
+and convert those into setting property values on an object that
+the program using the library defines, using attributes.
+
+Details:
+
+* Language: C# 3.0
+* Runtime: .NET 3.5 Client Profile
+
+Repository and project location: [CodePlex][1]  
+Maintainer: [Lasse V. Karlsen][2]
+
+  [1]: http://optnet.codeplex.com
+  [2]: mailto:lasse@vkarlsen.no
