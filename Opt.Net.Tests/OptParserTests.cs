09becbbc0ef8034878bd26ef6f1715a5b3c51fb7
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2010-12-29 16:00:30 +0100

    Added initial code for OptParser class with unit-tests.

---
+++ Opt.Net.Tests/OptParserTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Opt.Tests
+{
+    [TestFixture]
+    public class OptParserTests
+    {
+        [Test]
+        public void ParseGeneric_NullArguments_ThrowsArgumentNullException()
+        {
+            IEnumerable<string> arguments = null;
+
+            Assert.Throws<ArgumentNullException>(() => OptParser.Parse<OptParserTests>(arguments));
+        }
+
+        [Test]
+        public void ParseGeneric_WithEmptyArguments_ReturnsObjectOfRightType()
+        {
+            var container = OptParser.Parse<OptParserTests>(Enumerable.Empty<string>());
+
+            Assert.That(container, Is.Not.Null);
+            Assert.That(container, Is.InstanceOf(typeof (OptParserTests)));
+        }
+
+        [Test]
+        public void ParseWithContainerType_WithEmptyArguments_ReturnsObjectOfRightType()
+        {
+            object container = OptParser.Parse(typeof (OptParserTests), Enumerable.Empty<string>());
+
+            Assert.That(container, Is.Not.Null);
+            Assert.That(container, Is.InstanceOf(typeof (OptParserTests)));
+        }
+
+        [Test]
+        public void ParseWithContainer_NullArguments_ThrowsArgumentNullException()
+        {
+            var container = new object();
+            IEnumerable<string> arguments = null;
+
+            Assert.Throws<ArgumentNullException>(() => OptParser.Parse(container, arguments));
+        }
+
+        [Test]
+        public void ParseWithContainer_NullContainer_ThrowsArgumentNullException()
+        {
+            object container = null;
+            IEnumerable<string> arguments = Enumerable.Empty<string>();
+
+            Assert.Throws<ArgumentNullException>(() => OptParser.Parse(container, arguments));
+        }
+
+        [Test]
+        public void ParseWithType_NullArguments_ThrowsArgumentNullException()
+        {
+            Type containerType = typeof (OptParserTests);
+            IEnumerable<string> arguments = null;
+
+            Assert.Throws<ArgumentNullException>(() => OptParser.Parse(containerType, arguments));
+        }
+
+        [Test]
+        public void ParseWithType_NullContainerType_ThrowsArgumentNullException()
+        {
+            Type containerType = null;
+            IEnumerable<string> arguments = Enumerable.Empty<string>();
+
+            Assert.Throws<ArgumentNullException>(() => OptParser.Parse(containerType, arguments));
+        }
+    }
+}
---
+++ Opt.Net/StringEx.cs
@@ -0,0 +1,10 @@
+﻿namespace Opt
+{
+    internal static class StringEx
+    {
+        public static bool IsNullOrWhiteSpace(string value)
+        {
+            return value == null || value.Trim().Length == 0;
+        }
+    }
+}
