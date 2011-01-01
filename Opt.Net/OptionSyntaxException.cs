4ad80700798fb908eb45873c07d38404d6d0a5d7
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2011-01-01 22:49:30 +0100

    Fixed FxCop warnings; added parameterless default constructor for exception types.

--- Opt.Net/OptionSyntaxException.cs
+++ Opt.Net/OptionSyntaxException.cs
@@ -11,6 +11,14 @@ namespace Opt
     public class OptionSyntaxException : OptionException
     {
         /// <summary>
+        /// Initializes a new instance of the <see cref="OptionSyntaxException"/> class.
+        /// </summary>
+        public OptionSyntaxException()
+        {
+            // Do nothing here
+        }
+
+        /// <summary>
         /// Initializes a new instance of the <see cref="OptionSyntaxException"/> class with a specified error message.
         /// </summary>
         /// <param name="message">
@@ -51,31 +59,6 @@ namespace Opt
         protected OptionSyntaxException(SerializationInfo info, StreamingContext context)
             : base(info, context)
         {
-            // Do nothing here
-        }
-
-        /// <summary>
-        /// Sets the <see cref="SerializationInfo"/> with information about the exception.
-        /// </summary>
-        /// <param name="info">
-        /// The <see cref="SerializationInfo"/> that holds the serialized object data about the exception being thrown.
-        /// </param>
-        /// <param name="context">
-        /// The <see cref="StreamingContext"/> that contains contextual information about the source or destination.
-        /// </param>
-        /// <exception cref="ArgumentNullException">
-        /// The <paramref name="info"/> parameter is a null reference (Nothing in Visual Basic).
-        /// </exception>
-        /// <filterpriority>
-        /// 2
-        /// </filterpriority>
-        /// <PermissionSet>
-        ///   <IPermission class="System.Security.Permissions.FileIOPermission, mscorlib, Version=2.0.3600.0, Culture=neutral, PublicKeyToken=b77a5c561934e089" version="1" Read="*AllFiles*" PathDiscovery="*AllFiles*"/><IPermission class="System.Security.Permissions.SecurityPermission, mscorlib, Version=2.0.3600.0, Culture=neutral, PublicKeyToken=b77a5c561934e089" version="1" Flags="SerializationFormatter"/>
-        /// </PermissionSet>
-        public override void GetObjectData(SerializationInfo info, StreamingContext context)
-        {
-            base.GetObjectData(info, context);
-
             // Do nothing here
         }
     }
